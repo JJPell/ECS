237e1bd1142b03fca7fea586768eedfd1a1e4116
James Pell <13407273+JJPell@users.noreply.github.com>
2021-07-04 10:57:27 +0100

    Init

---
+++ System.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECS
+{
+    public abstract class System
+    {
+        public abstract bool Match(IComponent[] entityComponents);
+        public abstract void Run(KeyValuePair<Guid, IComponent[]> entity, int delta, int time);
+    }
+}
---
+++ World.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS;
+
+namespace ECS
+{
+    public class World
+    {
+        public Dictionary<Guid, IComponent[]> Entities = new Dictionary<Guid, IComponent[]>();
+
+        public Dictionary<Type, System> Systems = new Dictionary<Type, System>();
+
+        public World()
+        {
+
+        }
+
+        public Guid CreateEntity(IComponent[] components)
+        {
+            Guid id = Guid.NewGuid();
+            this.Entities[id] = components;
+            return id;
+        }
+
+        public IComponent[] GetComponents(Guid entity)
+        {
+            IComponent[] components;
+
+            if (!this.Entities.TryGetValue(entity, out components))
+            {
+                throw new Exception("Entity doesn't exist");
+            }
+
+            return components;
+        }
+
+        public Nullable<T> GetComponentByType<T>(Guid entity)
+        {
+            var components = this.GetComponents(entity);
+
+            foreach (var component in components)
+            {
+                if (component.GetType() == typeof(T))
+                {
+                    return (T?)component;
+                }
+            }
+
+            return null;
+        }
+
+        public void RegisterSystem(System system)
+        {
+            var type = system.GetType();
+
+            if (this.Systems.ContainsKey(type))
+            {
+                throw new Exception("System already registered");
+            }
+
+            this.Systems[type] = system;
+        }
+
+        public void ReplaceComponent(Guid entity, IComponent component)
+        {
+            var components = this.GetComponents(entity);
+
+            components.Append(component);
+
+            this.Entities[entity] = components;
+        }
+
+        public void Run(int delta, int time)
+        {
+            foreach (var system in this.Systems)
+            {
+                foreach (var entity in this.Entities)
+                {
+                    var components = entity.Value;
+                    var isMatch = system.Value.Match(components);
+                    if (!isMatch) continue;
+                    system.Value.Run(entity, delta, time);
+                }
+            }
+        }
+    }
+}
