fc0e2a43f2057f3580ed850e3e3d969cf8e6a38d
James Pell <13407273+JJPell@users.noreply.github.com>
2021-08-31 18:46:27 +0100

    Updated

--- System.cs
+++ System.cs
@@ -7,6 +7,6 @@ namespace ECS
     public abstract class System
     {
         public abstract bool Match(IComponent[] entityComponents);
-        public abstract void Run(KeyValuePair<Guid, IComponent[]> entity, int delta, int time);
+        public abstract void Run(KeyValuePair<int, IComponent[]> entity, int delta, int time, World world);
     }
 }
--- World.cs
+++ World.cs
@@ -9,28 +9,38 @@ namespace ECS
 {
     public class World
     {
-        public Dictionary<Guid, IComponent[]> Entities = new Dictionary<Guid, IComponent[]>();
+        public Dictionary<int, IComponent[]> Entities = new Dictionary<int, IComponent[]>();
 
         public Dictionary<Type, System> Systems = new Dictionary<Type, System>();
 
-        public World()
-        {
+        private int lastId = 0;
 
-        }
-
-        public Guid CreateEntity(IComponent[] components)
+        public int CreateEntity(IComponent[] components)
         {
-            Guid id = Guid.NewGuid();
+            var id = this.CreateId();
             this.Entities[id] = components;
             return id;
         }
 
-        public bool IsEntity(Guid entity) 
+        public bool IsEntity(int entity)
         {
             return this.Entities.ContainsKey(entity);
         }
 
-        public IComponent[] GetComponents(Guid entity)
+        public bool ComponentsContainComponent<T>(IComponent[] components)
+        {
+            foreach (var component in components)
+            {
+                if (component.GetType() == typeof(T))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IComponent[] GetComponents(int entity)
         {
             IComponent[] components;
 
@@ -48,19 +58,62 @@ namespace ECS
         /// <typeparam name="T">Component Type</typeparam>
         /// <param name="entity">Entity ID</param>
         /// <returns>A tuple with the first value being if the component exists on the entity and the second being the component</returns>
-        public (bool, T) GetComponentByType<T>(Guid entity)
+        public (bool, T) GetComponentByType<T>(int entity)
         {
             var components = this.GetComponents(entity);
 
             foreach (var component in components)
             {
-                if (component.GetType() == typeof(T))
+                var type1 = component.GetType();
+                var type2 = typeof(T);
+
+                if (type1 == type2)
                 {
                     return (true, (T)component);
                 }
             }
 
             return (false, default(T));
+        }
+
+        /// <summary>
+        /// Gets a component from an entiry by type
+        /// </summary>
+        /// <typeparam name="T">Component Type</typeparam>
+        /// <param name="components">Components</param>
+        /// <returns>A tuple with the first value being if the component exists on the entity and the second being the component</returns>
+        public (bool, T) GetComponentByType<T>(IComponent[] components)
+        {
+            foreach (var component in components)
+            {
+                var type1 = component.GetType();
+                var type2 = typeof(T);
+
+                if (type1 == type2)
+                {
+                    return (true, (T)component);
+                }
+            }
+
+            return (false, default(T));
+        }
+
+        public Dictionary<int, IComponent[]> GetEntitiesWithComponent<T>()
+        {
+            var entitiesWithComponent = new Dictionary<int, IComponent[]>();
+
+            foreach (var item in Entities)
+            {
+                var components = item.Value;
+                var containsComponent = ComponentsContainComponent<T>(components);
+
+                if (containsComponent)
+                {
+                    entitiesWithComponent.Add(item.Key, components);
+                }
+            }
+
+            return entitiesWithComponent;
         }
 
         public void RegisterSystem(System system)
@@ -75,11 +128,33 @@ namespace ECS
             this.Systems[type] = system;
         }
 
-        public void ReplaceComponent(Guid entity, IComponent component)
+        public void RemoveEntity(int entity)
         {
-            var components = this.GetComponents(entity);
+            Entities.Remove(entity);
+        }
 
-            components.Append(component);
+        public void ReplaceComponent(int entity, IComponent component)
+        {
+            IComponent[] components = this.GetComponents(entity);
+
+            bool updatedComponent = false;
+
+            for (int i = 0; i < components.Length; i++)
+            {
+                var iterationComponent = components[i];
+
+                if (iterationComponent.GetType() == component.GetType())
+                {
+                    components[i] = component;
+                    updatedComponent = true;
+                    break;
+                }
+            }
+
+            if (!updatedComponent)
+            {
+                components[components.Length] = component;
+            }
 
             this.Entities[entity] = components;
         }
@@ -93,9 +168,15 @@ namespace ECS
                     var components = entity.Value;
                     var isMatch = system.Value.Match(components);
                     if (!isMatch) continue;
-                    system.Value.Run(entity, delta, time);
+                    system.Value.Run(entity, delta, time, this);
                 }
             }
+        }
+
+        private int CreateId()
+        {
+            this.lastId++;
+            return this.lastId;
         }
     }
 }
